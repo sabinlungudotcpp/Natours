4833c8a29869b509c648bde8e6f25119ab5ecdca
Sabin Constantin Lungu <sabinlungu@Sabins-Mac-mini.broadband>
2021-03-27 16:43:51 +0000

    Refactored code and added routes and controllers folder

--- starter/controllers/userControllers.js
+++ starter/controllers/userControllers.js
@@ -3,17 +3,17 @@ exports.getAllUsers = (request, response) => {
 }
 
 exports.createUser = (request, response) => {
-
+    return response.status(500).json({message: 'Route not yet implemented'});
 };
 
 exports.getUserByID = (request, response) => {
-
+    return response.status(500).json({message: 'Route not yet implemented'});
 };
 
 exports.updateUserByID = (request, response) => {
-
+    return response.status(500).json({message: 'Route not yet implemented'});
 };
 
 exports.deleteUserByID = (request, response) => {
-
+    return response.status(500).json({message: 'Route not yet implemented'});
 }
