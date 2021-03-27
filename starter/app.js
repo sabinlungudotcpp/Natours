2edfb571ee0d31ecf34be1de1071a62e5763eb3c
Sabin Constantin Lungu <sabinlungu@Sabins-Mac-mini.broadband>
2021-03-27 11:24:20 +0000

    Adding CRUD operations

--- starter/app.js
+++ starter/app.js
@@ -1,19 +1,26 @@
 const express = require('express');
+const fs = require('fs');
 const app = express();
 const port = process.env.PORT || 3500;
 const okCode = 200;
 const notFound = 404;
+
+const tours = JSON.parse(fs.readFileSync('./dev-data/data/tours-simple.json'));
 
 app.get('/api/v1/tours', (request, response) => { // 1. GET ALL THE TOURS
     try {
         const method = request.method;
 
         if(method === 'GET') {
-
+            return response.status(okCode).json({
+                tours,
+                numberOfTours: tours.length
+            });
         }
     } 
     
     catch(error) {
+
         if(error) {
             return response.status(notFound).json({
                 message: error.toString()
@@ -23,6 +30,7 @@ app.get('/api/v1/tours', (request, response) => { // 1. GET ALL THE TOURS
 });
 
 app.listen(port, (error) => {
+
     if(!error) {
         return console.log(`Listening for requests on port ${port}`);
     }
