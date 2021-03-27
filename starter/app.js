ba9cd04339e7d97680d2411265bdfb9798b09752
Sabin Constantin Lungu <sabinlungu@Sabins-Mac-mini.broadband>
2021-03-27 15:24:51 +0000

    Adding CRUD operations (file baed)

--- starter/app.js
+++ starter/app.js
@@ -13,6 +13,7 @@ app.use(bodyParser.urlencoded({extended: false}));
 const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`));
 
 app.get('/api/v1/tours', (request, response) => { // 1. GET ALL THE TOURS
+
     try {
         const method = request.method;
 
@@ -37,11 +38,22 @@ app.get('/api/v1/tours', (request, response) => { // 1. GET ALL THE TOURS
 });  
 
 app.get('/api/v1/tours/:id', (request, response) => { // 2. GET A TOUR BY ID
-    console.log(request.params);
-    return response.status(okCode).json({})
+    const id = request.params.id * 1;
+    const tour = tours.find(el => el.id === id);
+
+    if(id > tours.length || !tour) {
+        return response.status(notFound).json({message: 'Tour Not Found'});
+    }
+
+    return response.status(okCode).json({
+        id,
+        data: {
+            tour
+        }
+    })
 });
 
-app.post('/api/v1/tours', (request, response) => {
+app.post('/api/v1/tours', (request, response) => { // Creates a new tour 
 
     try {
         const newId = tours[tours.length - 1].id + 1; // Get the last tour
@@ -59,13 +71,34 @@ app.post('/api/v1/tours', (request, response) => {
     } 
     
     catch(error) {
+
         if(error) {
             return response.json({
                 message: error.toString()
             });
         }
     }
-})
+});
+
+app.patch('/api/v1/tours/:id', (request, response) => {
+    const id = request.params.id * 1;
+    const tour = tours.find(el => el.id === id);
+
+    if(id > tours.length || !tour) {
+        return response.status(notFound).json({message: 'Tour Not Found'});
+    }
+
+    return response.status(okCode).json({
+        message: '<Updated Tour Success>',
+        sentAt: new Date().toISOString()
+    })
+});
+
+app.delete('/api/v1/tours/:id', (request, response) => {
+    return response.status(204).json({
+        data: undefined
+    })
+});
 
 app.listen(port, (error) => {
 
