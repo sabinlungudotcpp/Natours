1bda8c4847c7b39144a1aabb7ae82cda67722dff
Sabin Constantin Lungu <sabinlungu@Sabins-Mac-mini.broadband>
2021-03-27 09:18:09 +0000

    Adding starter files

--- starter/app.js
+++ starter/app.js
@@ -1,2 +1,13 @@
 const express = require('express');
 const app = express();
+const port = process.env.PORT || 3500;
+
+app.listen(port, (error) => {
+    if(!error) {
+        return console.log(`Listening for requests on port ${port}`);
+    }
+
+    else {
+        return console.error(error.toString());
+    }
+})
