ca1f1e230ecaf30fbcb43353f3b7d56f24d6f82d
Sabin Constantin Lungu <sabinlungu@Sabins-Mac-mini.broadband>
2021-03-28 21:15:24 +0100

    Finishing the tour model

--- starter/dev-data/data/import-dev-data.js
+++ starter/dev-data/data/import-dev-data.js
@@ -1,1 +1,22 @@
+const fs = require('fs');
+const Tour = require('../../models/TourModel');
 const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config({
+    path: './config.env'
+});
+
+const DB_CONNECTION = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
+
+mongoose.connect(DB_CONNECTION, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: true,
+    useUnifiedTopology: true
+}).then(() => {
+    console.log('Connected to Database Successfully')
+});
+
+// Read JSON file
+const tours = fs.readFileSync('tours-simple.json', 'utf-8');
